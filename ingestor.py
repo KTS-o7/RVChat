6e587cc0a82f45e27c43da367c5adb04b8a45f0f
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-02-27 21:44:48 +0530

    Initial Testing part 2

--- ingestor.py
+++ ingestor.py
@@ -1,5 +1,6 @@
 import os
 import warnings
+from tqdm import tqdm
 
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import (PyPDFLoader,CSVLoader, UnstructuredMarkdownLoader,UnstructuredPowerPointLoader,DirectoryLoader)
@@ -14,7 +15,6 @@ def create_vector_database():
     loadedDocuments = pdfDirecLoader.load()
     print(len(loadedDocuments))
     
-    
     textSplitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
     chunkedDocuments = textSplitter.split_documents(loadedDocuments)
     print(len(chunkedDocuments))
@@ -23,11 +23,7 @@ def create_vector_database():
     ollama_embeddings = OllamaEmbeddings(model='mistral')
     
     vectorDB = Chroma.from_documents(documents=chunkedDocuments,embedding=ollama_embeddings,persist_directory="./data")
-    #docs = vectorDB.similarity_search("Network",k=2)
-    #print(docs[0].page_content)
     
-    vectorDB.persist()
     
 if __name__ == "__main__":
     create_vector_database()
-    
